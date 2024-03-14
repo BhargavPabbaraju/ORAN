143605ed045a397f24e23deaae24e7b8d52759be
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2024-03-13 21:44:11 -0400

    accuracy shows %

--- views/accuracy.py
+++ views/accuracy.py
@@ -1,24 +1,32 @@
 
 from get_data  import Database
-from bokeh.models import Slider
-import random
+from bokeh.models import Slider,CustomJSTickFormatter
+from bokeh.layouts import column
+
+
 database = Database()
 
 
 
 def accuracy(doc):
     
-    
     def update():
         
         # Directly modify the text of 'div'
-        slider.value = database.get_accuracy()
-        
+        value = database.get_accuracy() 
+        slider.value = value
         
 
-    # Initialize 'div' here so that it's in the scope of 'update'
-    slider = Slider(start=0,end=100,value=10,bar_color='blue', disabled=True, sizing_mode="scale_width")
+        
     
+    # Initialize 'div' here so that it's in the scope of 'update'
+    slider = Slider(start=0,end=100,value=10,bar_color='blue', disabled=True, sizing_mode="scale_width",
+                    format=CustomJSTickFormatter(code="return tick.toFixed(2) + '%'"))
+    
+
+
+    #slider.js_on_change('value', callback)
+                        
 
     doc.add_root(slider)
     doc.add_periodic_callback(update, 500)  # Update every 250 ms
